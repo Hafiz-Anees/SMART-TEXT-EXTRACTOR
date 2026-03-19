291b0c51e8d6aaf80eafd718d06f062ef3359c1d
anees <aneesurrehman075@gmail.com>
2026-03-19 15:49:26 -0700

    "updated Ui"

--- ui.py
+++ ui.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
 def sidebar():
-    st.sidebar.title("📄 PDF Tool")
-    st.sidebar.write("Upload your PDF and extract text easily.")
+    st.sidebar.title("📄 Smart Text Extractor Tool")
+    st.sidebar.write("Upload your file and extract text easily.")
 
 def main_header():
-    st.title("✨ Smart PDF Text Extractor")
-    st.markdown("Upload your PDF and extract text instantly.")
+    st.title("✨ Smart Text Extractor")
+    st.markdown("Upload your file and extract text instantly.")
 
 def file_uploader():
-    return st.file_uploader("📂 Upload PDF", type=['pdf'])
+    return st.file_uploader("📂 Upload file", type=['pdf'])
 
 def show_results(text, pages):
     st.success("Extraction Complete ✅")
